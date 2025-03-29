16b6710e99ece162efc5cd8f09a5159596f6ee47
Nuno Mourinho <56609351+nunomourinho@users.noreply.github.com>
2025-03-29 19:34:00 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production', // evita o aviso sobre o mode
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    target: 'node',
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  target: 'node',
 };
